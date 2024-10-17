4827fbd969a56979e604cbc84ce8780fd6b491af
gaby-cz <gczarniak27@gmail.com>
2024-10-17 17:51:26 -0400

    Task 2 Lists and iteration

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -23,13 +23,21 @@ print ("is " + elevation + "' above sea level.")
 # /*--PS3: Code Block 2--*/
 
 # Subtask 2.1: Assign a variable named data_folder to the string object that prints as "W:\859_data\triangle"
+data_folder = r"W:\859_data\triangle"
+print(data_folder + "\n") # Added \n so that there's space between this and what is printed for Subtask 2.5
 
 # Subtask 2.2: Create a list object called data_list containing three string objects
+data_list = ["streams.shp", "stream_types.csv", "naip_imagery.tif"]
 
 # Subtask 2.3: Assign a variable named user_item and set it to the string "roads.shp"
+user_item = r"roads.shp"
 
 # Subtask 2.4: Add user_item string to the data_list list
+data_list.append(user_item)
 
-# Subtask 2.5: Loop through each item in data_list and for each object print full Windows path of each dataset
-    # Concatenate the data_folder string with the item in the data_list
+# Subtask 2.5: Loop through each item in data_list and for each object print full Windows path of each dataset    
     # Print output and check against PSet3 instructions
+for item in data_list:
+    # Concatenate the data_folder string with the item in the data_list
+    print(data_folder + "\\" + item)
+# %%
