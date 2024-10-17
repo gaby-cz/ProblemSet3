85319f54810b5a312913f914b6c3cfba5d97405e
gaby-cz <gczarniak27@gmail.com>
2024-10-17 13:07:08 -0400

    Part I Problem Set 3 Python syntax and string manipulation

---
+++ ProblemSet3_Part1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------
+# ProblemSet3_Part1.py
+#
+# Description: 
+# Python syntax & string manipulation
+# Lists and iteration
+#
+# Author: Gaby Czarniak (gabriella.czarniak@duke.edu)
+# Date:   October 2024
+#--------------------------------------------------------------
+
+#%% Task 1 - Edit code to print as requested
+# /*--PS3: Code Block 1--*/
+
+mountain = "Denali"
+nickname = 'Mt. McKinley'
+elevation = "20322" 
+
+print (mountain + ', sometimes \ncalled "' + nickname + '",')
+print ("is " + elevation + "' above sea level.")
+
+#%% Task 2 - Lists and Iteration
