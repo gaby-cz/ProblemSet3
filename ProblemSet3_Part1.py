f2c17bc599c154422d2a08f436b123120fe5ffbc
gaby-cz <gczarniak27@gmail.com>
2024-10-17 13:13:44 -0400

    Commented outline of subtasks for Task 2

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -2,8 +2,8 @@
 # ProblemSet3_Part1.py
 #
 # Description: 
-# Python syntax & string manipulation
-# Lists and iteration
+# Task 1: Python syntax & string manipulation
+# Task 2: Lists and iteration
 #
 # Author: Gaby Czarniak (gabriella.czarniak@duke.edu)
 # Date:   October 2024
@@ -20,3 +20,16 @@ print (mountain + ', sometimes \ncalled "' + nickname + '",')
 print ("is " + elevation + "' above sea level.")
 
 #%% Task 2 - Lists and Iteration
+# /*--PS3: Code Block 2--*/
+
+# Subtask 2.1: Assign a variable named data_folder to the string object that prints as "W:\859_data\triangle"
+
+# Subtask 2.2: Create a list object called data_list containing three string objects
+
+# Subtask 2.3: Assign a variable named user_item and set it to the string "roads.shp"
+
+# Subtask 2.4: Add user_item string to the data_list list
+
+# Subtask 2.5: Loop through each item in data_list and for each object print full Windows path of each dataset
+    # Concatenate the data_folder string with the item in the data_list
+    # Print output and check against PSet3 instructions
